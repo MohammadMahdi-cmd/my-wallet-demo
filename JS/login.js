c1d630e4da3c34ffe41605fbf3dc1ddfa4388fa1
MohammadMahdi-cmd <mohammadmahdi.web.dev@gmail.com>
2025-05-06 12:21:39 +0600

    Initial commit

---
+++ JS/login.js
@@ -0,0 +1,19 @@
+// Step 1 = ID k dhorsi getelementbyId diye
+document.getElementById("login-btn").addEventListener('click',
+    function(event){
+        event.preventDefault();
+        const accountNumber = document.getElementById("account-number").value
+        const pin = document.getElementById("PIN").value
+        if(accountNumber.length === 11){
+            if(pin.length === 4){
+                window.location.href = "./main.html"
+            }
+            else{
+                console.log("invalid PIN number")
+            }
+        }
+        else{
+            console.log("invalid account number")
+        }
+    }
+)
